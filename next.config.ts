94abd8c9f15735dc0f1df9caac6cb1b5517e39a6
EzmOdeus <shafrax500@gmail.com>
2025-03-29 09:12:20 +0200

    V1

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverActions: true,
-    serverComponentsExternalPackages: [],
+    serverActions: true, // لو عايز تستخدمها
   },
-  
+  serverExternalPackages: [],
+
   images: {
     domains: ["assets.coingecko.com"],
   },
